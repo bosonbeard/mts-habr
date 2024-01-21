825298364f9d980b952cfd88e49bece5661066d5
BosonBeard <bosonbeard@gmail.com>
2024-01-22 01:52:43 +0300

    added send request logic

--- wedding_invitation/send_voice.py
+++ wedding_invitation/send_voice.py
@@ -9,7 +9,7 @@ import json
 # Set variables 
 # They uses only in send_request function and placed here for easier setup
 # Don't forget to paste your MTS EXolve API token and arended phone bellow
-token = "Type your API token here" 
+token = 'Type here arended MTS exolve phone' 
 arended_phone = "Type here arended MTS exolve phone"
 # change this vars if API path will changed
 base_url = "https://api.exolve.ru"
@@ -18,33 +18,11 @@ urls={
     "get_info":f"{base_url}/call/v1/GetInfo",
     "send_sms":f"{base_url}/SendSMS"
     }
-head = f"Authorization: Bearer {token}"
+headers = {
+    "Content-Type": "application/json", 
+    "Authorization": f"Bearer {token}"
+}
 
-
-
-
-
-
-def send_voice_messages():
-    name = "";
-    
-
-    with open(in_filepath, 'r', encoding='utf-8-sig', newline='') as infile:
-        reader = csv.reader(infile)
-        next(reader, None)  # skip the headers
-        for item in reader:
-           text_template = get_text_template(name)
-           phone = item[1]
-           if (text_template and phone ):
-                result = send_request(number=item)
-                id = result["modelos"][0]["codigo"] #change to real api
-                print(writerow([id, item[0], item[1] ] ))
-                print(id)
-           else:
-               next(reader, None);  # skip the broken record
-               print(["Error: not correct data", item[0], item[1] ] )
-                
-                
 
 
 
@@ -55,23 +33,34 @@ def read_csv(in_filepath):
         reader = csv.reader(infile)
         next(reader, None)  # skip the headers
         for item in reader:
-            guests_list.append(item)
+            if (item[0], item[1]):
+                guests_list.append(item)
             else:
                 print(["Error: not correct data", item ] )
     return guests_list
 
 
-def send_request(number):
-    url = "https://parallelum.com.br/fipe/api/v1/carros/marcas/59/modelos"
-    response = requests.request("GET", url)
-  
-    return response.json()
+def send_request(type,body):
+    result="";
+    if type in urls:
+        url = urls[type]
+        payload = json.dumps(body)
+        response = requests.request("POST", url, headers=headers, data=payload)
+        result = response.json()
+        print(response.text)
+    else:
+        print("Unknown request type")
+    return result
 
-def send_voice_messages(phones):
+
+
+
+
+def send_voice_messages(guests_list):
     name = "";
 
-def check_listened()
-
+def check_listened():
+    pass
 
 def send_sms(in_filepath):
     template = f"Дорогие {name}, приглашаем вас к нам свадьбу 01.01.2024! Ждем вас с нетерпением, ваши Иван да Марья"
@@ -87,35 +76,20 @@ def returnMatches(a, b):
 """
     
 
-    with open(in_filepath, 'r', encoding='utf-8-sig', newline='') as infile:
-        reader = csv.reader(infile)
-        next(reader, None)  # skip the headers
-        for item in reader:
-           text_template = get_text_template(name)
-           phone = item[1]
-           if (text_template and phone ):
-                result = send_request(number=item)
-                id = result["modelos"][0]["codigo"] #change to real api
-                print(writerow([id, item[0], item[1] ] ))
-                print(id)
-           else:
-               next(reader, None);  # skip the broken record
-               print(["Error: not correct data", item[0], item[1] ] )
-
-
-
 if __name__ == '__main__':
     # parse path to csv file (deault - guest_list.csv.csv)
     parser = argparse.ArgumentParser()
-    parser.add_argument('--inpath', type=str, required=False, default='guest_list.csv.csv')    
+    parser.add_argument('--inpath', type=str, required=False, default='guest_list.csv')    
     print ("start")
     args = parser.parse_args()
 
     guests_list= read_csv(args.inpath)
-    success_voice_sent,error_voice_sent=send_voice_messages(guests_list)
-    listen_list, need_sms_list = check_listened (success_voice_sent)
-    success_sms_sent, error_sms_sent = send_sms(need_sms_list)
-    print(f"I have {card.price}")
-    print(f"I have {card.price}")
-    print(f"I have {card.price}")
+   # print (guests_list)
+    send_request("get_info",{"call_id": "caldf32f642-6313-4dae-94db-98502ad3249c"})
+  #  success_voice_sent,error_voice_sent=send_voice_messages(guests_list)
+  #  listen_list, need_sms_list = check_listened (success_voice_sent)
+  #  success_sms_sent, error_sms_sent = send_sms(need_sms_list)
+    print(f"I have {guests_list}")
+    print(f"I have {guests_list}")
+    print(f"I have {guests_list}")
 
