053650a17abde414d259517e9ef7a95ca23a7c4b
BosonBeard <bosonbeard@gmail.com>
2024-01-23 01:06:03 +0300

    added function logics

--- wedding_invitation/send_voice.py
+++ wedding_invitation/send_voice.py
@@ -4,19 +4,21 @@ import requests
 import csv
 import argparse
 import json
+import time
 
 
 # Set variables 
 # They uses only in send_request function and placed here for easier setup
-# Don't forget to paste your MTS EXolve API token and arended phone bellow
-token = 'Type here arended MTS exolve phone' 
-arended_phone = "Type here arended MTS exolve phone"
+# Don't forget to paste your MTS EXolve API token, arended phone, and message id bellow
+token = 'Type your API token here' 
+arended_phone = "700000000000"
+message_id="78987f5b-c281-4d23-be2b-fb7a0402126d"
 # change this vars if API path will changed
 base_url = "https://api.exolve.ru"
 urls={
-    "send_voice":f"{base_url}/voice-message/v1/MakeVoiceMessage",
+    "send_voice":f"{base_url}/call/v1/MakeVoiceMessage",
     "get_info":f"{base_url}/call/v1/GetInfo",
-    "send_sms":f"{base_url}/SendSMS"
+    "send_sms":f"{base_url}/messaging/v1/SendSMS"
     }
 headers = {
     "Content-Type": "application/json", 
@@ -28,12 +30,16 @@ headers = {
 
 
 def read_csv(in_filepath):
+    """
+    Read data from csv with columns [id, name, phone] \r \n
+    in_filepath - path to csv \r \n
+    """
     guests_list =[]
     with open(in_filepath, 'r', encoding='utf-8-sig', newline='') as infile:
         reader = csv.reader(infile)
         next(reader, None)  # skip the headers
         for item in reader:
-            if (item[0], item[1]):
+            if (item[0], item[2]):
                 guests_list.append(item)
             else:
                 print(["Error: not correct data", item ] )
@@ -41,40 +47,87 @@ def read_csv(in_filepath):
 
 
 def send_request(type,body):
+    """
+    Sending request to MTS Exolve Api \r \n
+    type - api method for request (see urls keys above) \r \n
+    body - payload for POST method
+    """
     result="";
     if type in urls:
         url = urls[type]
+        print(url)
         payload = json.dumps(body)
         response = requests.request("POST", url, headers=headers, data=payload)
+        print(response)
         result = response.json()
-        print(response.text)
     else:
         print("Unknown request type")
     return result
 
 
 
-
-
 def send_voice_messages(guests_list):
-    name = "";
+    """
+    Sending voice messages by MTS Exolve Api \r \n
+    guests_list - list with next structure [id, name, phone] \r \n
+    """
+    success_voice_list = [];
+    error_voice_list = [];
+    for guest in guests_list:
+        body = {
+            "source": f"{arended_phone}",
+            "destination": f"{guest[2]}",
+            "service_id": f"{message_id}"
+        }
+        result= send_request("send_voice",body)
+        if ( "call_id" in result):
+            success_voice_list.append([guest[0],result["call_id"]])
+        else:
+            error_voice_list.append(guest)
+    return success_voice_list, error_voice_list
 
-def check_listened():
-    pass
+def check_listened(success_voice_list):
+    """
+    Checking status of voice messages by MTS Exolve Api \r \n 
+    success_voice_list - list of previous success sended voice messages \r \n
+    """
+    listen_list  = [];
+    need_sms_list = [];
+    for voice in success_voice_list:
+        body = {
+            "call_id": f"{success_voice[1]}"
+        }
+        result= send_request("voice",body)
+        if ( "status" in result): 
+            if  send_request["status"]=="completed":
+                listen_list.append(voice[0])
+        else:
+            need_sms_list.append(voice[0])
+    return listen_list, need_sms_list
 
-def send_sms(in_filepath):
-    template = f"Дорогие {name}, приглашаем вас к нам свадьбу 01.01.2024! Ждем вас с нетерпением, ваши Иван да Марья"
-
-"""
-123456
-def returnMatches(a, b):
-    matches = []
-    for i in a:
-        if i in b:
-            matches.append(i)
-    return matches
-"""
-    
+def send_sms(guests_list, need_sms_list):
+    """
+    Sending SMS by MTS Exolve Api \r \n
+    guests_list - list with next structure [id, name, phone] \r \n
+    need_sms_list - list with ids guests whom we will sent SMS \r \n
+    """
+    success_sms_list = [];
+    error_sms_list = [];
+    for guest in guests_list:
+        for id in need_sms_list:
+            if guest[0] == id:
+                body = {
+                    "number": f"{arended_phone}",
+                    "destination": f"{guest[2]}",
+                    "text": f"{guest[1]} ждем на нашу свадьбу! Таня + Володя"
+                }
+                result= send_request("send_sms",body)
+                if ( "message_id" in result):
+                    success_sms_list.append([guest[0],result["call_id"]])
+                else:
+                    error_sms_list.append(guest)
+                break
+    return success_sms_list, error_sms_list
 
 if __name__ == '__main__':
     # parse path to csv file (deault - guest_list.csv.csv)
@@ -82,14 +135,11 @@ if __name__ == '__main__':
     parser.add_argument('--inpath', type=str, required=False, default='guest_list.csv')    
     print ("start")
     args = parser.parse_args()
-
     guests_list= read_csv(args.inpath)
-   # print (guests_list)
-    send_request("get_info",{"call_id": "caldf32f642-6313-4dae-94db-98502ad3249c"})
-  #  success_voice_sent,error_voice_sent=send_voice_messages(guests_list)
-  #  listen_list, need_sms_list = check_listened (success_voice_sent)
-  #  success_sms_sent, error_sms_sent = send_sms(need_sms_list)
-    print(f"I have {guests_list}")
-    print(f"I have {guests_list}")
-    print(f"I have {guests_list}")
+    success_voice_list,error_voice_list=send_voice_messages(guests_list)
+    time.sleep(10)    # Pause 5.5 seconds
+    listen_list, need_sms_list = check_listened (success_voice_list)
+    success_sms_list, error_sms_list = send_sms(guests_list, need_sms_list)
+  #  print(f"I have {guests_list}")
+
 
