fa27b782c7fdfd7702fd8928e7e41f23d5c4c5c3
BosonBeard <bosonbeard@gmail.com>
2025-03-22 23:31:01 +0300

    fixed log addr

--- barrier_control/api/config.py
+++ barrier_control/api/config.py
@@ -14,7 +14,7 @@ LOGGING = {
     'handlers': {
         'file': {
             'class': 'logging.FileHandler',
-            'filename': './app.log',
+            'filename': 'app.log',
             'formatter': 'simple'
         },
     },
